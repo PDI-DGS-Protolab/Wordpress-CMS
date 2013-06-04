842b2852e361abe3ec4b7b4351e694b4c1cafcba
Alberto Blázquez Rodríguez <hola@albertoblazquez.net>
2013-06-04 12:47:34 +0200

    Upload Instant Servers Theme

---
+++ wp-content/themes/instant_servers/footer.php
@@ -0,0 +1,34 @@
+<?php $STATIC_URL = "./"; ?>
+
+      <footer>
+            <div class="center">
+
+                <ul id="menu-pie">
+                    <li class="telefonica">&copy; Telefónica S.A.&nbsp;</li>
+                    <li> | <a target="_blank" href="http://www.telefonica.com/en/legal_text/proteccion_datos.shtml">All rights reserved</a>&nbsp;</li>
+                    <li> | <a target="_blank" href="http://www.telefonica.com/en/about_telefonica/html/home/home.shtml">About Telefónica</a>&nbsp;</li>
+                    <li> | <a target="_blank" href="http://www.telefonica.com/en/contact_us/corporativo.shtml">Contact us</a>&nbsp;</li>
+                    <li> | <a target="_blank" href="http://www.telefonica.com/en/legal_text/requisitos.shtml">Requirements</a>&nbsp;</li>
+                    <li> | <a target="_blank" href="http://www.telefonica.com/en/legal_text/declara_accesibilidad.shtml">Accessibility</a>&nbsp;</li>
+                    <!--li> | <a target="_blank" href="http://www.telefonica.com/en/legal_text/proteccion_datos.shtml">Privacy Policy</a>&nbsp;</li!-->
+                </ul>
+
+            </div>
+        </footer>
+    </div>
+
+
+    <!-- analytics -->
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-34423639-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+
+</body>
---
+++ wp-content/themes/instant_servers/header.php
@@ -0,0 +1,25 @@
+<?php $STATIC_URL = "./"; ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Instant Servers | Cloud servers that evolve with your business | Telef&oacute;nica</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+        <link rel="stylesheet" type="text/css" href="<?php $STATIC_URL . 'css/jquery-ui-1.8.20.custom.css'?>" />
+        <link rel="stylesheet" type="text/css" href="<?php $STATIC_URL . 'css/jquery.fancybox-1.3.4.css'?>" media="screen" />
+        <link rel="stylesheet" type="text/css" href="<?php $STATIC_URL . 'css/stylesheet.css'?>" />
+        <link rel="stylesheet" type="text/css" href="<?php $STATIC_URL . 'css/style.css'?>" />
+        <link rel="shortcut icon" type="image/ico" href="<?php $STATIC_URL . 'img/favicon.ico'?>" />
+</head>
+
+<body>
+    <div id="alto">
+        <header>
+            <div class="center">
+                <a href="http://www.telefonica.com/en/home/jsp/home.jsp" target="_blank" title="Telefónica" > <div id="telefonica_logo" class="ir" >Telefónica</div></a>
+                <a href="index.html"><h1 id="instant_servers">Instant Servers</h1></a>
+            </div>
+        </header>
